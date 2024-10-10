485057fbdb9cfe82ce7fc10d5b32e60da2c4f4f9
Blaze34536 <88854346+Blaze34536@users.noreply.github.com>
2024-10-09 21:57:44 -0500

    gets list of questions

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 from PIL import Image
 import pytesseract
 questions = []
+cleaned_list=[]
 
 # Path to the image
-image_path = r'..\Grade-o-matic-1\input\math.png'
+image_path = r'..\Grade-o-matic-1\input\math.jpg'
 
 # Open the image using PIL
 image = Image.open(image_path)
 
 # Use pytesseract to extract text from the image
 text = pytesseract.image_to_string(image)
-print(text)## TESTING PURPOSES## 
+
+questions.append(text.split("="))
+for question in questions:
+    for q in question:
+        cleaned_list.append(q.strip())
+
+
+## TESTING PURPOSES## 
+print(text)
+print(cleaned_list)
