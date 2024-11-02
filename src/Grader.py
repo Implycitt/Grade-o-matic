89e7a29b3ba74c987167454cb42b5a9efd40d95a
Blaze34536 <88854346+Blaze34536@users.noreply.github.com>
2024-11-01 20:45:37 -0500

    Update Grader.py

--- src/Grader.py
+++ src/Grader.py
@@ -1,4 +1,4 @@
 def Grade(evaluated_list, answers_list):
-    equal_count = sum(1 for a, e in zip(answers_list, evaluated_list) if a == e)
+    equal_count = sum(1 for a, e in zip(answers_list, evaluated_list) if a == e) ## checks how many times answer and evaluated are the same
     total = len(evaluated_list)
     return (equal_count/total)*100
